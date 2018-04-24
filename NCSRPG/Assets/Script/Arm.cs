4b66591b9ad3352da053e954ed9c9d523b4ddb67
downkhg <37098799+downkhg@users.noreply.github.com>
2018-04-24 18:11:16 +0900

    Player
    Player Action

---
+++ NCSRPG/Assets/Script/Arm.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//코루틴을 사용하지않고 공격하기
+public class Arm : MonoBehaviour {
+    float m_fAttackSpeed;
+    float m_fMaxRot = 45;
+    float m_fCurRot = 0;
+    bool m_bAttack = false;
+    bool m_bRelease = false;
+
+    private void Update()
+    {
+        if (m_bRelease)
+            ReleaseArmUpdate();
+        else if (m_bAttack)
+            AttackArmUpdate();
+    }
+
+    void MoveArm(float rotSpeed)
+    {
+        transform.Rotate(Vector3.right * rotSpeed);
+    }
+
+    //코루틴을 사용하지않은 경우 멤버변수로 만들어, 
+    //매프레임마다 호출확인을 해야한다.
+    void AttackArmUpdate()
+    {
+        if (m_fCurRot < m_fMaxRot)
+        {
+            MoveArm(m_fAttackSpeed);
+            m_fCurRot += m_fAttackSpeed;
+            Debug.Log(string.Format("CurRot:{0}/{1}", m_fCurRot, m_fMaxRot));
+        }
+        else
+        {
+            m_fCurRot = 0;
+            m_bRelease = true;
+        }
+    }
+
+    void ReleaseArmUpdate()
+    {
+        if (m_fCurRot < m_fMaxRot)
+        {
+            MoveArm(-m_fAttackSpeed);
+            m_fCurRot += m_fAttackSpeed;
+            Debug.Log(string.Format("CurRot:{0}/{1}", m_fCurRot, m_fMaxRot));
+        }
+        else
+        {
+            m_bAttack = false;
+            m_bRelease = false;
+            m_fCurRot = 0;
+        }
+    }
+
+    public void Attack()
+    {
+        if (m_bAttack == false)
+            m_bAttack = true;
+    }
+
+    public void Init(float Attack)
+    {
+        m_fAttackSpeed = Attack;
+    }
+}
--- NCSRPG/Assets/Script/Dynamic.cs
+++ NCSRPG/Assets/Script/Dynamic.cs
@@ -7,10 +7,13 @@ public class Dynamic : MonoBehaviour {
     public GameObject m_objArm;
     public int m_fMoveSpeed;
     public int m_fRotSpeed;
+    public Arm m_cArm;
+    bool m_bAttack;
+   
     // Use this for initialization
     void Start () {
-		
-	}
+        m_cArm.Init(m_fRotSpeed);
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -34,15 +37,62 @@ public class Dynamic : MonoBehaviour {
         m_objArm.transform.Rotate(Vector3.right * rotSpeed);
     }
 
+    
+
+    //코루틴을 사용하면, 매 프레임마다 반복문의 한스탭이 실행되므로,
+    //따로 끝날때 처리를 하지않아도되고, 해당반복의 스탭이 끝나면 실행이 끝난다.
+    //실행중 지역변수들은 코루틴이 끝날때까지 유지된다.
+    IEnumerator AttackArm()
+    {
+        float fMaxRot = 45;
+        float fCurRot = 0;
+
+        while(fCurRot  < fMaxRot)
+        {
+            //매프레임마다 실행
+            MoveArm(m_fRotSpeed);
+            fCurRot += m_fRotSpeed;
+            yield return null; //다음 프레임으로.
+        }
+        
+        StartCoroutine("ReleaseArm");
+    }
+
+    IEnumerator ReleaseArm()
+    {
+        float fMaxRot = 45;
+        float fCurRot = 0;
+
+        while (fCurRot < fMaxRot)
+        {
+            MoveArm(-m_fRotSpeed);
+            fCurRot += m_fRotSpeed;
+            yield return null;
+        }
+
+        m_bAttack = false;
+    }
+
     //케릭터이동 만들기. 키입력을하여만듦
     void InputProcess()
     {
         float fMoveDist = m_fMoveSpeed * Time.deltaTime;
         float fRotAngle = m_fRotSpeed * Time.deltaTime;
 
+        //if (m_bAttack)
+        //    AttackArmUpdate();
+
         if (Input.GetMouseButtonDown(0))
         {
-            MoveArm(m_fRotSpeed);
+            //MoveArm(m_fRotSpeed);
+            //그냥 코루틴을 이용시에는 코루틴을 부를때마다 
+            //해당 반복문이 실행되므로, 중복해서 계속 동작한다.
+            //if (m_bAttack == false)
+            //{
+            //    StartCoroutine("AttackArm");
+            //    m_bAttack = true;
+            //}
+            m_cArm.Attack();
         }
         if (Input.GetKey(KeyCode.W))
         {
