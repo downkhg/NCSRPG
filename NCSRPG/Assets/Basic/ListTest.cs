4ff66ea67d3083fb93c2635fa5fa0ad5b10c0505
downkhg <37098799+downkhg@users.noreply.github.com>
2018-05-15 15:22:36 +0900

    Push

---
+++ NCSRPG/Assets/Basic/ListTest.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ListTest : MonoBehaviour {
+
+    struct StructA
+    {
+        public int nData;
+    }
+
+    class ClassA
+    {
+        public int nData;
+    }
+
+    public GameObject m_objObject;
+
+    public List<GameObject> m_listGameObjects = new List<GameObject>();
+
+    List<GameObject> GetList()
+    {
+        return m_listGameObjects;
+    }
+
+    void SwapStruct(StructA a, StructA b)
+    {
+        int temp = a.nData;
+        a.nData = b.nData;
+        b.nData = temp;
+    }
+
+    void SwapClass(ClassA a, ClassA b)
+    {
+        int temp = a.nData;
+        a.nData = b.nData;
+        b.nData = temp;
+    }
+
+    // Use this for initialization
+    void Start () {
+        m_listGameObjects = new List<GameObject>();
+        m_listGameObjects.Add(Instantiate(m_objObject));
+        m_listGameObjects.Add(Instantiate(m_objObject));
+        m_listGameObjects.Add(Instantiate(m_objObject));
+        m_listGameObjects.Add(Instantiate(m_objObject));
+        List<GameObject> list = GetList();
+        for(int i = 0; i < list.Count; i++ )
+        {
+            //Destroy(list[i]);
+            Debug.Log("list.Count"+ list.Count);
+        }
+        list.Clear();
+
+        ClassA cA = new ClassA();
+        cA.nData = 10;
+        ClassA cB = new ClassA();
+        cB.nData = 20;
+        StructA sA,sB;
+        sA.nData = 10;
+        sB.nData = 20;
+        Debug.Log(string.Format("struct:{0}/{1}\n",sA.nData,sB.nData));
+        Debug.Log(string.Format("class:{0}/{1}\n", cA.nData, cB.nData));
+        SwapClass(cA, cB);
+        SwapStruct(sA, sB);
+        Debug.Log(string.Format("struct-Swap:{0}/{1}\n", sA.nData, sB.nData));
+        Debug.Log(string.Format("class-Swap:{0}/{1}\n", cA.nData, cB.nData));
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ NCSRPG/Assets/Push/DynamicPush.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DynamicPush : MonoBehaviour {
+    Rigidbody m_cRigidbody;
+    public float m_fPower;
+    public float m_fSpeed = 10.0F;
+    public float m_fRotSpeed = 100.0F;
+
+    // Use this for initialization
+    void Start () {
+        m_cRigidbody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        float fVertical = Input.GetAxis("Vertical");
+        float fHorizontal = Input.GetAxis("Horizontal");
+
+        if (Mathf.Abs(fVertical) > 0 || Mathf.Abs(fHorizontal) > 0)
+        {
+            float fTranslation = fVertical * m_fSpeed;
+            float fRotation = fHorizontal * m_fRotSpeed;
+            fTranslation *= Time.deltaTime;
+            fRotation *= Time.deltaTime;
+            transform.Translate(0, 0, fTranslation);
+            transform.Rotate(0, fRotation, 0);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        GameObject objTarget = collision.gameObject;
+        Rigidbody rigidbodyTarget = objTarget.GetComponent<Rigidbody>();
+        rigidbodyTarget.AddForce(transform.forward * m_fPower * m_fSpeed);
+    }
+}
